db13795fcdc8df02ef603fc5e440cbcb9c748eee
My PC <vanduyho717@gmail.com>
2025-03-19 15:09:13 +0700

    category/controller-danhsach

--- app/models/Category.php
+++ app/models/Category.php
@@ -1,1 +1,16 @@
-<!-- Models -->
+<?php 
+    require_once '../app/configs/Database.php';
+    class Category extends Database{
+        private $table = "category";
+        public function get_all_category() {
+            $sql = "SELECT * FROM $this->table";
+            return $this->select($sql);
+        }
+
+        public function add_category($url, $img_url, $user_id) {
+            $sql = "INSERT INTO $this->table (url, img_url, user_id) 
+                    VALUES (?, ?, ?)";
+            return $this->execute($sql, [$url, $img_url, $user_id]);
+        }
+    }
+?>
