866b6f95d08725c7677093896e4f7f1d2b624f34
Duy Anh <huynhduyanh01011996@gmail.com>
2025-03-21 23:45:00 +0700

    duy anh brand

--- app/controllers/BrandController.php
+++ app/controllers/BrandController.php
@@ -1,17 +1,57 @@
 <?php
- require_once '../app/models/Brand.php';
+   require_once '../app/models/Brand.php';
+
     class BrandController {
+        // public function __construct() {
+        //     $this->productModel = new Products();
         private $brandModel;
         public function __construct() {
-            $this->brandModel = new Brands();
+            $this->brandModel = new Brand();
         }
+        // }
         public function index() {
-            $brands = $this -> brandModel -> all_brand();
+            // Lấy tất cả thương hiệu
+            $brands = $this->brandModel->get_all_brands();
             $content = '../app/views/pages/admin/brand.php';
             include_once "../app/views/layouts/admin.php";
-            
         }
+
+        public function add_brand(){
+            if(isset ($_POST["add_brand"]) ){
+                $name_brand = $_POST['name_brand'];
+                $hinh_anh = $_POST['hinh_anh'];
+                if($name_brand === "" || $hinh_anh === ""){
+                    echo ` <script>
+                                aler('vui lòng điền đủ từ ')
+                            </script>`;
+                }else{
+                    $brandModel->$this->brandModel->add_brand( $name_brand, $hinh_anh);
+                }
+            }
+            
+            // Thêm một thương hiệu mới
+            
+            $content = '../app/views/pages/admin/brand.php';
+            include_once "../app/views/layouts/admin.php";
+        }
+
+        public function update_brand(){
+            // Cập nhật thương hiệu
+            $brandModel->$this->brandModel->update_brand(1, "Nike", "nike_updated.jpg");
+            $content = '../app/views/pages/admin/brand.php';
+            include_once "../app/views/layouts/admin.php";
+        }
+
+        public function delete_brand($id){
+            // Xóa thương hiệu
+            $brandModel->$this->brandModel->delete_brand(3);
+            $content = '../app/views/pages/admin/brand.php';
+            include_once "../app/views/layouts/admin.php";
+        }       
 
     }
 
 ?>
+
+ 
+
--- app/views/pages/admin/brand.php
+++ app/views/pages/admin/brand.php
@@ -1,1 +1,73 @@
-<h2>Brand</h2>
+
+<div class="w-100"></div>
+<div class="row">
+    <form method="POST" class="col-4">
+        <h5 class="mb-3">Form nhập thương hiệu</h5>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Tên thuơng hiệu </label>
+            <input type="text" class="form-control" name="name_brand" id="exampleFormControlInput1" placeholder="Nhập đường dẫn tại đây">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Chọn phương thức nhập ảnh</label>
+            <div>
+                <input type="radio" id="urlOption" name="imageOption" checked>
+                <label for="urlOption">Nhập URL</label>
+                <input type="radio" id="fileOption" name="imageOption">
+                <label for="fileOption">Tải lên tệp</label>
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <label for="imageUrl" class="form-label">Đường dẫn URL</label>
+            <input type="text" class="form-control" name="hinh_anh" id="imageUrl" placeholder="Nhập URL hình ảnh">
+        </div>
+
+        <div class="mb-3">
+            <label for="imageFile" class="form-label">Hình ảnh</label>
+            <input class="form-control" type="file" name="hinh_anh" id="imageFile" disabled>
+        </div>
+        <div class="mb-3 text-end">
+            <button type="button" name="add_brand" class="btn btn-primary">Thêm ngay</button>
+        </div>
+    </form>
+   
+
+    <div class="col-8">
+        <table class="table">
+            <thead>
+                <tr class="text-center">
+                    <th scope="col">Id</th>
+                    <th scope="col">Hình ảnh</th>
+                    <th scope="col">Đường dẫn</th>
+                    <th scope="col">Tùy chọn</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                        $content = '';
+                        foreach ($brands as $key => $brand) {
+                            $content .= '
+                                <tr class="text-center">
+                                    <th scope="row">'.($key + 1).'</th>
+                                    <td>
+                                        <img class="mx-auto" width="200" height="100"
+                                            src="'.$brand['img_url'].'"
+                                            alt="">
+                                    </td>
+                                    <td><a target="_blank" href="'.$brand['url'].'">Link</a></td>
+                                    <td>
+                                        <button class="btn btn-danger">Xóa</button>
+                                        <button class="btn btn-primary">Sửa</button>
+                                    </td>
+                                </tr>
+                            ';
+                        }
+                    ?>
+                <?php echo $content ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
+
+
