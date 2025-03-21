768e4ec4bda6e877a2636d0491f7486dfdfba369
kittenwarrior-qb <kittenwarrior2005@gmail.com>
2025-03-21 15:30:16 +0700

    Sửa default.php,home.php

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -24,13 +24,14 @@
         top: 0;
         left: 0;
         width: 100%;
-        background: white;
-        transition: transform 0.3s ease-in-out;
+        background: transparent;
+        transition: background 0.3s ease-in-out, transform 0.3s ease-in-out;
         z-index: 1000;
     }
 
-    .header-hidden {
-        transform: translateY(-100%);
+    .header-scroll {
+        background: white;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
     }
 </style>
 
@@ -93,14 +94,43 @@
             </div>
 
             <div class="d-flex gap-3">
-                <a href="#"><img src="./img/search.svg" width="20"></a>
-                <a href="#"><img src="./img/heart.svg" width="20"></a>
-                <a href="#"><img src="./img/user.svg" width="20"></a>
-                <a class="position-relative" href="#">
-                    <img src="./img/cart.svg" width="20">
-                    <!-- <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill ">3</span> -->
-                </a>
+                <div class="">
+                    <a href="#"><img src="./img/search.svg" width="20"></a>
+                </div>
+
+                <div class="">
+                    <a href="#"><img src="./img/heart.svg" width="20"></a>
+                </div>
+
+                <div class="dropdown">
+                    <a href="#" data-bs-toggle="dropdown"><img src="./img/user.svg" width="20"></a>
+                    <ul class="dropdown-menu text-center" style="left: 50%; transform: translateX(-50%); width: max-content;">
+                        <?php
+                        if (isset($_SESSION['email'])) {
+                        ?>
+                            <li><a class="dropdown-item" href="#">Thông tin tài khoản</a></li>
+                            <li><a class="dropdown-item" href="#">Quản lý đơn hàng</a></li>
+                            <li><a class="dropdown-item text-danger" href="logout.php">Đăng xuất</a></li>
+                        <?php
+                        } else {
+                        ?>
+                            <li><a class="dropdown-item" href="login.php">Đăng nhập</a></li>
+                            <li><a class="dropdown-item" href="register.php">Đăng kí</a></li>
+                        <?php
+                        }
+                        ?>
+                    </ul>
+                </div>
+
+
+                <div class=" position-relative">
+                    <a href="#">
+                        <img src="./img/cart.svg" width="20">
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">3</span>
+                    </a>
+                </div>
             </div>
+
         </nav>
     </header>
 
@@ -122,8 +152,14 @@
         const header = document.querySelector("header");
 
         window.addEventListener("scroll", () => {
+            if (window.scrollY > 50) {
+                header.classList.add("header-scroll");
+            } else {
+                header.classList.remove("header-scroll");
+            }
+
             if (window.scrollY > lastScrollY) {
-                header.classList.add("header-hidden");
+                header.classList.add("header-hidden"); // Ẩn header khi cuộn xuống
             } else {
                 header.classList.remove("header-hidden");
             }
