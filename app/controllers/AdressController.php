40c552075960967c25d9c96809d58b48b8496063
Nguyen Minh Quan <minhhquann1508@gmail.com>
2025-04-24 00:30:46 +0700

    fix order

--- app/controllers/AdressController.php
+++ app/controllers/AdressController.php
@@ -1,11 +1,19 @@
 <?php
+    require_once '../app/models/Address.php';
     class AdressController {
-        // private $productModel;
-        // public function __construct() {
-        //     $this->productModel = new Products();
-        // }
-        public function index() {
-            echo '<div>Đây là trang danh sách địa chỉ</div>';
+        private $addressModel;
+        public function __construct() {
+            $this->addressModel = new Address();
         }
+        public function add_address () {
+            $ward = $_POST['ward'] ?? '';
+            $district = $_POST['district'] ?? '';
+            $city = $_POST['city'] ?? '';
+            $street = $_POST['street'] ?? '';
+            $user_id = $_POST['user_id'];
+            $response = $this->addressModel->add($ward, $district, $city, $street, $user_id);
+            echo json_encode($response);
+            exit;
+        } 
     }
 ?>
